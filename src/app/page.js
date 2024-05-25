96b2213f023851806b49a3be0bde33d569390f95
Hamza E. <32574974+e-hamza@users.noreply.github.com>
2024-05-25 21:31:47 +0000

    Components Navigation added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,15 @@
 import Notification from "./components/Notification";
 import Modal from "./components/Modal";
+import { ArrowRight } from "@phosphor-icons/react/dist/ssr"
 
 export default function Home() {
     return (
-        <div className="flex flex-col space-y-8">
+        <div className="flex flex-col space-y-12">
+            <div className="flex flex-row items-center space-x-6">
+                <h1 className="font-plus-jakarta-sans bg-slate-900 text-white font-bold text-2xl rounded-full py-6 px-16 cursor-pointer hover:bg-slate-800">Components</h1>
+                <ArrowRight color="#000000" weight="regular" size={40} />
+                <h1 className="font-plus-jakarta-sans border-4 border-neutral-900 text-black font-bold text-2xl rounded-full py-6 px-16 cursor-pointer hover:bg-slate-200">Alerts & Notifications</h1>
+            </div>
             <h1 className="text-6xl text-slate-800 font-bold">Notification</h1>
             <p className="text-slate-500">Notifications are like little digital poke-ins (without the actual effort of poking). Deliver your message, but don't expect them to jump up right away.</p>
             <Modal />
