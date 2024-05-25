27fd74020677f6afef981351bffa0ca277ba281d
ilias <annouri.ilias@gmail.com>
2024-05-25 16:49:32 +0100

    initial commit

---
+++ src/app/components/Modal.js
@@ -0,0 +1,7 @@
+export default function Modal(){
+    return(
+        <div className="w-32 h-32 bg-green-500">
+
+        </div>
+    )
+}
---
+++ src/app/components/Notification.js
@@ -0,0 +1,7 @@
+export default function Notification(){
+    return(
+        <div className="w-32 h-32 bg-blue-500">
+
+        </div>
+    )
+}
