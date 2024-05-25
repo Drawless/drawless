ca7c7816113a3fabaedfe030edef8960216b4789
ilias <annouri.ilias@gmail.com>
2024-05-25 22:33:03 +0100

    modals

--- src/app/components/Modal.js
+++ src/app/components/Modal.js
@@ -1,5 +1,7 @@
+import Image from 'next/image'
 import { Info, X } from "@phosphor-icons/react/dist/ssr"
-export default function Modal(){
+import { ButtonContained, ButtonOutlined } from "./Button"
+export function ModalBasic(){
     const title="Insert Your Modal Title"
     const text="A lazy fox jumps over the bloody cyborg, gets its data stolen to the cybernet."
     return(
@@ -21,12 +23,45 @@ export default function Modal(){
                 </div>
             </div>
             <div className="flex justify-end  space-x-2 mt-8">
-                <div className="flex items-center justify-center w-[82px] h-[40px] border-[1px] border-gray-400 rounded-3xl cursor-pointer hover:bg-gray-100">
-                    <span className="font-bold text-sm text-gray-500">Cancel</span>
+                <ButtonOutlined text="Cancel" />
+                <ButtonContained text="Accept" color="indigo"/>
+            </div>
+        </div>
+    )
+}
+
+export function ModalImage(){
+    const title="Insert Your Modal Title"
+    const text="A lazy fox jumps over the bloody cyborg, gets its data stolen to the cybernet."
+    return(
+        <div className="min-h-[280px] w-[500px] rounded-3xl shadow-lg p-6 bg-white">
+            <div className="flex justify-between">
+                <div className="flex items-center justify-center w-12 h-12 rounded-full bg-slate-100 cursor-help">
+                    <Info color="#4F46E5" weight="bold" size={24} />
                 </div>
-                <div className="flex items-center justify-center w-[82px] h-[40px] bg-indigo-600 rounded-3xl cursor-pointer hover:bg-indigo-500">
-                    <span className="font-bold text-sm text-white">Accept</span>
+                <div className="flex items-center justify-center h-8 w-8 rounded-full cursor-pointer hover:bg-gray-100">
+                    <X color="#475569" weight="bold" size={24} />
                 </div>
+            </div>
+            <div className="mt-[24px] space-y-2 min-h-20">
+                <div>
+                    <span className="font-extrabold text-gray-800 text-xl">{title}</span>
+                </div>
+                <div>
+                    <span className="font-regular text-base text-gray-500">{text}</span>
+                </div>
+            </div>
+            <div className="h-[240px] w-[452px] rounded-3xl bg-gray-200 mt-6">
+                <Image
+                src="/profile.png"
+                width={500}
+                height={500}
+                alt="Picture of the author"
+                />
+            </div>
+            <div className="flex justify-end  space-x-2 mt-8">
+                <ButtonOutlined text="Cancel" />
+                <ButtonContained text="Accept" color="indigo"/>
             </div>
         </div>
     )
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
 import Notification from "./components/Notification";
-import Modal from "./components/Modal";
+import { ModalBasic, ModalImage } from "./components/Modal";
 
 export default function Home() {
     return (
         <div className="flex flex-col space-y-8">
             <h1 className="text-6xl text-slate-800 font-bold">Notification</h1>
             <p className="text-slate-500">Notifications are like little digital poke-ins (without the actual effort of poking). Deliver your message, but don't expect them to jump up right away.</p>
-            <Modal />
+            <ModalBasic />
+            <ModalImage />
         </div>
 );
 }
