6f81d96b0de32f8862268706c5101773c7459e07
Hamza E. <32574974+e-hamza@users.noreply.github.com>
2024-05-25 16:14:50 +0000

    Started Layout

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,13 @@ import Modal from "./components/Modal";
 
 export default function Home() {
     return (
-        <div className="flex min-h-screen flex-col items-center justify-between p-24 bg-yellow-500">
-            <Notification />
-            <Modal />
+        <div className="w-full min-h-screen bg-white">
+            <div className="container m-auto rounded-[64px] bg-slate-100 text-black">
+                <div className="p-48 font-plus-jakarta-sans flex flex-col space-y-8">
+                    <h1 className="text-6xl text-slate-800 font-bold">Notification</h1>
+                    <p className="text-slate-500">Notifications are like little digital poke-ins (without the actual effort of poking). Deliver your message, but don't expect them to jump up right away.</p>
+                </div>
+            </div>
         </div>
 );
 }
