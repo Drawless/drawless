fe979dda5bc25c03fd1d03736c7eeb6b7d1f0657
Hamza E. <32574974+e-hamza@users.noreply.github.com>
2024-05-30 09:54:39 +0000

    feat(design): Added Disabled File Upload Inputs

--- src/app/components/file_upload/page.js
+++ src/app/components/file_upload/page.js
@@ -14,8 +14,8 @@ export default function FileUpload() {
 
             <div className="w-full min-h-[1px] bg-slate-200"> </div>
             <div className="flex flex-col space-y-10">
-                <div className="flex flex-col items-center space-y-8 border-2 border-slate-200 border-dashed rounded-3xl p-12 cursor-pointer hover:bg-gray-100">
-                    <div className="rounded-full bg-slate-200 p-6">
+                <div className="flex flex-col items-center space-y-8 border-2 border-slate-200 border-dashed rounded-3xl p-12 cursor-pointer hover:bg-[#F8FAFC]">
+                    <div className="rounded-full bg-[#EEF2FF] p-6">
                         <FileArrowUp color="#4F46E5" weight="bold" size={38} />
                     </div>
                     <div className="flex flex-col space-y-2 items-center">
@@ -23,13 +23,32 @@ export default function FileUpload() {
                         <h2 className="text-slate-400">Supported Format: SVG, JPG, PNG (10mb each)</h2>
                     </div>
                 </div>
-                <div className="flex flex-col items-center space-y-8 border-2 border-slate-200 border-dashed rounded-3xl p-12 cursor-pointer hover:bg-gray-100">
+                <div className="flex flex-col items-center space-y-8 border-2 border-slate-200 border-dashed rounded-3xl p-12 cursor-pointer hover:bg-[#F8FAFC]">
                     <div className="flex flex-col space-y-2 items-center">
                         <h2 className="text-blue-600 font-bold">Browse your file to upload!</h2>
                         <h2 className="text-slate-400">Supported Format: SVG, JPG, PNG (10mb each)</h2>
                     </div>
                     <div className="bg-blue-600 flex flex-row justify-center space-x-4 rounded-full px-8 py-4 font-bold">
                         <h1>Browse File</h1>
+                        <UploadSimple color="#ffffff" weight="bold" size={24} />
+                    </div>
+                </div>
+                <div className="flex flex-col items-center space-y-8 border-2 border-slate-300 border-dashed rounded-3xl p-12 cursor-not-allowed bg-[#F8FAFC] select-none">
+                    <div className="rounded-full bg-slate-100 p-6 border border-slate-300">
+                        <FileArrowUp color="#4F46E5" weight="bold" size={38} />
+                    </div>
+                    <div className="flex flex-col space-y-2 items-center">
+                        <h2 className="text-slate-300 font-medium"><span className="text-blue-300 font-bold">Click here</span> to upload your file or drag and drop.</h2>
+                        <h2 className="text-slate-400">Supported Format: SVG, JPG, PNG (10mb each)</h2>
+                    </div>
+                </div>
+                <div className="flex flex-col items-center space-y-8 border-2 border-slate-200 border-dashed rounded-3xl p-12 cursor-not-allowed bg-[#F8FAFC] select-none">
+                    <div className="flex flex-col space-y-2 items-center">
+                        <h2 className="text-blue-300 font-bold">Browse your file to upload!</h2>
+                        <h2 className="text-slate-400">Supported Format: SVG, JPG, PNG (10mb each)</h2>
+                    </div>
+                    <div className="bg-blue-100 flex flex-row justify-center space-x-4 rounded-full px-8 py-4 font-bold">
+                        <h1 className="text-blue-400">Browse File</h1>
                         <UploadSimple color="#ffffff" weight="bold" size={24} />
                     </div>
                 </div>
